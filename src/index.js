3e42ca11a8806cf766463b46ecaa51ae36490a59
Gunn-Treepaech <treepaech@kkumail.com>
2022-11-10 16:52:24 +0700

    add NewV.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 // external packages
 // npm install googleapis@105 @google-cloud/local-auth@2.1.0 --save
 const express = require("express");
-require("dotenv").config();
 const {google} = require('googleapis');
 const keys = require('./keys.json');
+const diff = require('dialogflow-fulfillment');
 
+require("dotenv").config();
 //--------------------------------------------------------------------------
 // Website
 // https://developers.google.com/sheets/api/quickstart/nodejs
 // https://www.youtube.com/watch?v=MiPpQzW_ya0
+// https://learn.microsoft.com/en-us/office/dev/add-ins/excel/excel-add-ins-tables
 //-------------------------------------------------------------------------
 
 // Start the webapp
@@ -31,11 +33,27 @@ webApp.get("/", (req, res) => {
 });
 
 webApp.post("/webhook", (req, res) => {
-// Get answer form Dialogflow
-  let answer = req.body.queryResult.fulfillmentText;
-  //console.log(answer);
+  let msg = req.body.queryResult.queryText;
+  if (msg == 'คำถามที่พบบ่อย'){
+    //console.log(req.body);
+    console.log(JSON.stringify( req.body.queryResult.fulfillmentMessages));
+    customPayload(req);
+    console.log(JSON.stringify( req.body.queryResult.fulfillmentMessages));
+  } else {
+    addToSheet(req);
+  }
+});
 
-// Sheet API --------------------------------------------------------------
+// Start the server
+webApp.listen(PORT, () => {
+  console.log(`Server is up and running at ${PORT}`);
+});
+
+async function addToSheet(request){
+  // Get answer form Dialogflow
+  let answer = request.body.queryResult.fulfillmentText;
+//console.log(answer);
+  // Sheet API --------------------------------------------------------------
   const client = new google.auth.JWT(
     keys.client_email,
     null,
@@ -48,7 +66,7 @@ webApp.post("/webhook", (req, res) => {
         console.log(err);
         return;
     } else {
-        console.log('Connected!');
+        console.log('Successfully connected to Sheet!');
         gsrun(client);
     }
   });
@@ -71,20 +89,30 @@ webApp.post("/webhook", (req, res) => {
     //console.log(dataArraySheet2);
 
 // Check Answer --------------------------------------------------------------
-    console.log(answer);
+    //console.log(answer);
     let numberOfAnswer;
+    let testV1;
     //console.log(dataArraySheet2[1][1]);
     for (let i = 0; i < dataArraySheet2.length; i++) {
       //console.log(dataArraySheet2[i]);
       let AnsweQ = dataArraySheet2[i][1];
       if (answer == AnsweQ){
         numberOfAnswer = i;
+        testV1 = dataArraySheet1[i][1];
         //console.log(i);
       }
     }
 // ---------------------------------------------------------------------------
 // Update NumberOfTimesAsked -------------------------------------------------
-    if (numberOfAnswer != null){
+    let NewdataSheet1 = await gsapi.spreadsheets.values.get(optSheet1);
+    let NewdataArraySheet1 = NewdataSheet1.data.values;
+    let cValue = NewdataArraySheet1[numberOfAnswer][1];
+    //console.log(testV1);
+    //console.log(cValue);
+    if (testV1 != cValue){
+      let upDateAsked = Number(cValue) + 1;
+      dataArraySheet1[numberOfAnswer][1] = upDateAsked;
+    } else {
       let asked = dataArraySheet1[numberOfAnswer][1];
       let upDateAsked = Number(asked) + 1;
       dataArraySheet1[numberOfAnswer][1] = upDateAsked;
@@ -99,9 +127,30 @@ webApp.post("/webhook", (req, res) => {
     //console.log(resSheet);
 // ---------------------------------------------------------------------------
   }
-});
+}
 
-// Start the server
-webApp.listen(PORT, () => {
-  console.log(`Server is up and running at ${PORT}`);
-});
+function customPayload(req){
+  let payloadData =  [
+    {
+      "payload": {
+        "line": {
+          "type": "text",
+          "text": "กรุณาเลือกรายการ",
+         "quickReply": { 
+          "items": [
+            {
+              "type": "action", 
+              "imageUrl": "https://www.apivat.com/line-chatbot/img-icon.png",
+              "action": {
+                "type": "message",
+                "label": "มัธยมศึกษาปีที่ 8",
+                "text": "มัธยมศึกษาปีที่ 8"
+              }
+            }]
+          }
+        }
+      }      
+    }];
+  req.body.queryResult.fulfillmentMessages = payloadData;
+  //console.log(JSON.stringify( req.body.queryResult.fulfillmentMessages));
+}
